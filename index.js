4ac100a23b6f7bb01da5a89a77fd47bd89663478
Alex <alex@alex-dev.me>
2024-01-09 14:30:02 -0600

    Add initial server setup

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+
+// Express server
+const app = express();
+
+// DB connection
+dbConnection();
+
+// CORS
+app.use(cors())
+
+// Public directory
+app.use(express.static('public'));
+
+// Read and parse body
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+// 404
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + '/public/404.html');
+});
+
+// TODO: CRUD events
+
+
+
+// Listen requests
+app.listen(process.env.PORT, () => {
+    console.log(`Running on port ${process.env.PORT}`);
+});
